1f8c745381cdaa1cef1fe6c56aa0c5dcd08cc22e
zedl00 <nestorvaldez@hotmail.com>
2022-10-12 20:18:28 -0400

    Plantilla PrimeVue Sakai Instalada

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import App from '../App.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,31 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: App,
+      children: [
+        {
+          path: '',
+          name: 'dashboard',
+          component: () => import('../components/Dashboard.vue')
+        },
+      ]
+    },
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import('../pages/Login.vue')
+        },
+        {
+          path: '/landing',
+          name: 'landing',
+          component: () => import('../pages/LandingDemo.vue')
+        }
+
+
   ]
 })
 
